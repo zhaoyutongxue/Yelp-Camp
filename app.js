920eb497966f3fc9322a703f131089d3b6368d37
Zhaoyu(Henry) Zhong <50900868+zhaoyutongxue@users.noreply.github.com>
2022-06-06 23:06:33 +1000

    414. campground show
    campground show page done.
    show.ejs under folder campgrounds.
    It is used to show each one of the campground information.

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose');
-const path =require('path')
+const path = require('path')
 const port = 3000
 const Campground = require('./models/campground.js')
 
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp',{
-    useNewUrlParser:true,
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
     // useCreateIndex:true,
-    useUnifiedTopology:true
+    useUnifiedTopology: true
 })
 
 // from the document: 
 const db = mongoose.connection;
-db.on('error',console.error.bind(console,"connection error:"));
-db.once("open",()=>{
+db.on('error', console.error.bind(console, "connection error:"));
+db.once("open", () => {
     console.log("database connected")
 })
 
@@ -25,35 +25,41 @@ const app = express()
 
 // use EJS for HTML templating: use a template to render data. \
 // set view engine to EJS.
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 // __dirname is an environment variable that tells you the absolute path of the directory containing the currently executing f
-app.set('views',path.join(__dirname,'/views'))
+app.set('views', path.join(__dirname, '/views'))
 
-app.get('/campgrounds',async(req,res) => {
+//List of the campgrounds:
+app.get('/campgrounds', async (req, res) => {
     // save mongo db data into a local variable, then pass through the data and render it. 
     const campgrounds = await Campground.find();
-    res.render('campgrounds/index', {campgrounds})
+    res.render('campgrounds/index', { campgrounds })
 })
-
+//Show 1 campground: 
+app.get('/campgrounds/:id', async (req, res) => {
+    const campID = req.params.id;
+    const campground = await Campground.findById(campID);
+    res.render('campgrounds/show', { campID, campground })
+})
 
 app.get('/home', (req, res) => {
     res.render('home')
-  })
-
-app.get('/makecampground', async(req, res) => {
-// make a camp instance(it is a Document of Model Campground),
-// now it is just an object in javascript, it needs to be saved.
-  const camp = new Campground({title:'my backyard', description:'just your backyard '})
-  await camp.save();
-  res.send(camp); 
 })
- 
+
+app.get('/makecampground', async (req, res) => {
+    // make a camp instance(it is a Document of Model Campground),
+    // now it is just an object in javascript, it needs to be saved.
+    const camp = new Campground({ title: 'my backyard', description: 'just your backyard ' })
+    await camp.save();
+    res.send(camp);
+})
+
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World! from Yelp Camp')
+    res.send('Hello World! from Yelp Camp')
 })
 
 app.listen(port, () => {
-  console.log(`Yelp camp app listening on port ${port}`)
+    console.log(`Yelp camp app listening on port ${port}`)
 })
