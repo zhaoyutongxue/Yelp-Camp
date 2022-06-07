e2b5d62f3bedcd79f01f10d1df3e43aa2f8b35c1
Zhaoyu(Henry) Zhong <50900868+zhaoyutongxue@users.noreply.github.com>
2022-06-07 23:25:14 +1000

    415. campground new and create
    1. add new.ejs, it is a page to allow user add new campground.
    2. add code to take the data from the form post method, and save the data to database.

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useUnifiedTopology: true
 })
 
-// from the document: 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, "connection error:"));
 db.once("open", () => {
@@ -29,12 +28,29 @@ app.set('view engine', 'ejs');
 // __dirname is an environment variable that tells you the absolute path of the directory containing the currently executing f
 app.set('views', path.join(__dirname, '/views'))
 
+
 //List of the campgrounds:
 app.get('/campgrounds', async (req, res) => {
     // save mongo db data into a local variable, then pass through the data and render it. 
     const campgrounds = await Campground.find();
     res.render('campgrounds/index', { campgrounds })
 })
+
+// page to add new campground
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
+// this line of coder allow you to see the req.body.
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+    // res.send(req.body)
+})
+
 //Show 1 campground: 
 app.get('/campgrounds/:id', async (req, res) => {
     const campID = req.params.id;
@@ -42,20 +58,12 @@ app.get('/campgrounds/:id', async (req, res) => {
     res.render('campgrounds/show', { campID, campground })
 })
 
+// campground home page
 app.get('/home', (req, res) => {
     res.render('home')
 })
 
-app.get('/makecampground', async (req, res) => {
-    // make a camp instance(it is a Document of Model Campground),
-    // now it is just an object in javascript, it needs to be saved.
-    const camp = new Campground({ title: 'my backyard', description: 'just your backyard ' })
-    await camp.save();
-    res.send(camp);
-})
-
-
-
+// homepage 
 app.get('/', (req, res) => {
     res.send('Hello World! from Yelp Camp')
 })
