0a0a59fd01de3001edb46120bcdfa742231407ab
Zhaoyu(Henry) Zhong <zhaoyuzhong2015@gmail.com>
2022-06-30 16:26:31 +1000

    513.finish register user

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.use(session({
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }))
+// flash message middleware
 app.use(flash())
 app.use((req, res, next) => {
     res.locals.success = req.flash('success')
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js')
-
+const catchAsync = require('../utils/catchAsync')
 
 router.get('/register', (req, res) => {
     res.render('./users/register.ejs');
 })
-router.post('/register', async (req, res) => {
-    // const user = new User(req.body )
-    res.send(req.body)
-})
+router.post('/register', catchAsync(async (req, res) => {
+    try {
+        const { email, username, password } = req.body
+        const user = new User({ email: email, username: username });
+        const newUser = await User.register(user, password);
+        req.flash('success', 'welcome to yelp-camp!')
+        res.redirect('/campgrounds')
+    } catch (e) {
+        req.flash('error', e.message)
+        res.redirect('/register')
+
+    }
+
+}))
 module.exports = router;
