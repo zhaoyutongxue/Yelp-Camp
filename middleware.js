944d3565df8c417b529cf7a52a959e4b0404d0a6
Zhaoyu(Henry) Zhong <zhaoyuzhong2015@gmail.com>
2022-07-13 11:41:02 +1000

    hardcode 1 user with admin access

--- middleware.js
+++ middleware.js
@@ -17,7 +17,7 @@ module.exports.ensureLogin = (req, res, next) => {
 module.exports.isAuthor = async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
-    if (!campground.author._id.equals(req.user._id)) {
+    if (!campground.author._id.equals(req.user._id)&&!req.user._id.equals('62cd578ffa7d9f6208b2dbbe')) {
         req.flash('error', 'you dont have permission to do that')
         return res.redirect(`/campgrounds/${id}`)
     }
@@ -27,7 +27,7 @@ module.exports.isAuthor = async (req, res, next) => {
 module.exports.isReviewAuthor = async (req, res, next) => {
     const { id, reviewId } = req.params;
     const review = await Review.findById(reviewId);
-    if (!review.author._id.equals(req.user._id)) {
+    if (!review.author._id.equals(req.user._id)&&!req.user._id.equals('62cd578ffa7d9f6208b2dbbe')) {
         req.flash('error', 'you dont have permission to do that')
         return res.redirect(`/campgrounds/${id}`)
     }
